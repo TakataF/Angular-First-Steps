ab6b5708cc369954b5f71377ed76832ef77f8365
Felipe <felipetakatadealvarenga@gmail.com>
2023-05-10 18:15:07 -0300

    Aula de Componente pai e filho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "Joaquim";//irei compartilhar esse dado com outro componente
+
   title = 'y';
 }
